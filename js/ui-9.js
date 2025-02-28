5693e336ae8cd350c18c6bbcfb412c0140527fee
juistespastelastes <juistespastelastes@gmail.com>
2025-02-28 22:08:26 +0100

    Update ui-9.js

--- js/ui-9.js
+++ js/ui-9.js
@@ -1,3 +1,4 @@
+// Обработка имен
 const getname = document.querySelectorAll('#getname');
 const neddeing = document.querySelectorAll('#neddeing');
 
@@ -6,3 +7,30 @@ getname.forEach((element, index) => {
         neddeing[index].value = getname[index].textContent.trim();
     });
 });
+
+// Стили для диалоговых окон
+const styles = {
+    desktop: {
+        position: 'absolute',
+        left: 'auto',
+        top: '90px'
+    },
+    mobile: {
+        position: 'absolute', 
+        top: '50px',
+        left: '0px'
+    }
+};
+
+// Применяем стили
+document.querySelector('[data-reach-dialog-content].cnHaYL').style.cssText = `
+    position: ${styles.desktop.position};
+    left: ${styles.desktop.left};
+    top: ${styles.desktop.top};
+`;
+
+document.querySelector('[data-reach-dialog-content].iDXOlP').style.cssText = `
+    position: ${styles.mobile.position};
+    top: ${styles.mobile.top}; 
+    left: ${styles.mobile.left};
+`;
